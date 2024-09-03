6f5cabd7e8e8b2ea2947c1e8593dd196d1265c8c
ACE-wil <118677338+ACE-wil@users.noreply.github.com>
2024-09-03 15:26:31 +0800

    安全可复用版
    安全可复用版

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,6 +6,7 @@ const Page = async (res:any) => {
     const user = await getUser();
     
     if (!user || !user.id) redirect(res,302,'auth-callback?origin=dashboard')
+        // 如果用户不存在，跳转到登录页面
 
     return <div>{user.email}</div>
 }
