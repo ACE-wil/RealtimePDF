47210969dd44395d193c065537ca6ac171f42d9d
ACE-wil <118677338+ACE-wil@users.noreply.github.com>
2024-09-03 23:38:36 +0800

    增加数据删除功能
    增加数据删除功能

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -3,6 +3,7 @@ import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
 import { privateProcedure, publicProcedure, router } from './trpc';
 import { TRPCError } from '@trpc/server';
 import { db } from '@/db';
+import { z } from 'zod'
 
 export const appRouter = router({
     // Api端点1：身份验证/公共回调
@@ -43,7 +44,26 @@ export const appRouter = router({
                     userId
                 }
             })
-        })
+        }),
+
+    // Api端点3：删除文件/私密
+        deleteFile: privateProcedure.input(
+            z.object({ id: z.string() })
+        ).mutation(async ({ctx, input}) => {
+            const { userId } = ctx
+
+            const file = await db.file.findFirst({
+                where: {
+                    id: input.id,
+                    userId,
+                },
+            })
+
+            if (!file) throw new TRPCError({ code: 'NOT_FOUND' });
+
+            await db.file.delete({ where: { id: input.id } });
+            return file
+        }),
 
 });
 
